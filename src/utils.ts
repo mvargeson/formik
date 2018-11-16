934a689b6062caa622805e6e50d9e6b783b2491d
Matthew Vargeson <matt.vargeson@outreach.io>
2018-11-16 12:17:06 -0800

    Fix the illegal invocation in setNested... util with try/catch

--- src/utils.ts
+++ src/utils.ts
@@ -79,18 +79,25 @@ export function setNestedObjectValues<T>(
   response: any = {}
 ): T {
   for (let k of Object.keys(object)) {
-    const val = object[k];
-    if (isObject(val)) {
-      if (!visited.get(val)) {
-        visited.set(val, true);
-        // In order to keep array values consistent for both dot path  and
-        // bracket syntax, we need to check if this is an array so that
-        // this will output  { friends: [true] } and not { friends: { "0": true } }
-        response[k] = Array.isArray(val) ? [] : {};
-        setNestedObjectValues(val, value, visited, response[k]);
+    try {
+      const val = object[k];
+      if (isObject(val)) {
+        if (!visited.get(val)) {
+          visited.set(val, true);
+          // In order to keep array values consistent for both dot path  and
+          // bracket syntax, we need to check if this is an array so that
+          // this will output  { friends: [true] } and not { friends: { "0": true } }
+          response[k] = Array.isArray(val) ? [] : {};
+          setNestedObjectValues(val, value, visited, response[k]);
+        }
+      } else {
+        response[k] = value;
       }
-    } else {
-      response[k] = value;
+    } catch (e) {
+      console.warn(
+        `Error fetching key '${k}' in Formik#setNestedObjectValues`,
+        e
+      ); // eslint-disable-line no-console
     }
   }
 
